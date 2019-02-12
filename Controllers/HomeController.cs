079c0677e736be6c77b30756dea5278ef5c20843
Randy Park <randyp@smu.edu>
2019-02-12 09:35:36 -0500

    Session object store

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using redistester.Models;
+
+namespace redistester.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            //Load data from distributed data store asynchronously
+            await HttpContext.Session.LoadAsync();
+            //Get value from session
+            string storedValue = HttpContext.Session.GetString("TestValue");
+            if (storedValue == null)
+            {
+                //No value stored, set one
+                storedValue = "Testing session in Redis. Time of storage: " + DateTime.Now.ToString("s");
+                HttpContext.Session.SetString("TestValue", storedValue);
+                //Store session data asynchronously
+                await HttpContext.Session.CommitAsync();
+            }
+            ViewData["Message"] = "Value in session: " + storedValue;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace redistester.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
